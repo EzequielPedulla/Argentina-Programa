f8bccdbc205ffe8584870992d2f8cf161548b0f0
Ezequiel Pedulla <ivanpedulla@gmail.com>
2023-01-27 00:29:16 -0300

    Agregando alerta js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     mensaje: "",
   };
   // Seleccionar los elementos de la interfaz
-  const inputNombre = document.querySelector("#name");
+  const inputNombre = document.querySelector("#nombre");
   const inputEmail = document.querySelector("#email");
   const inputMensaje = document.querySelector("#mensaje");
   const formulario = document.querySelector("#formulario");
@@ -14,7 +14,27 @@ document.addEventListener("DOMContentLoaded", function () {
   inputNombre.addEventListener("blur", validar);
   inputEmail.addEventListener("blur", validar);
   inputMensaje.addEventListener("blur", validar);
+
   function validar(e) {
-    console.log(e.target.value);
+    if (e.target.value.trim() === "") {
+      mostrarAlerta(`el Campo ${e.target.id} es obligatorio`);
+    } else {
+      console.log("si hay algo");
+    }
+  }
+
+  function mostrarAlerta(mensaje) {
+    //Comprueba si ya existe na alerta
+    const alerta = document.querySelector(".alerta");
+    if (alerta) {
+      alerta.remove();
+    }
+    //Generar alerta en HTMl
+    const error = document.createElement("P");
+    error.textContent = mensaje;
+    error.classList.add("alerta");
+
+    //inyectar el error al formulario
+    formulario.appendChild(error);
   }
 });
